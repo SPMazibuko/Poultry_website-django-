3785e200cb4aae4cf35d11cdbbbdeae08c3b20bb
SPMazibuko <spmazibuko07@gmail.com>
2022-10-05 07:24:43 +0100

    updated styles

--- poultryEnv/Scripts/activate.bat
+++ poultryEnv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Sifiso\Desktop\Poultry_website\poultryEnv
+set VIRTUAL_ENV=C:\Users\TUT\Desktop\Poultry_website-django-\poultryEnv
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -25,6 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
+set VIRTUAL_ENV_PROMPT=(poultryEnv) 
 
 :END
 if defined _OLD_CODEPAGE (
